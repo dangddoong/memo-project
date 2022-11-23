ff1b9cc47d2d1c55557bf4fbc90af5edf38dae2e
yichi <coding2210@gmail.com>
2022-11-23 19:10:08 +0900

    update error code
    숫자 입력이 안 될 시 오류가 뜨면서 종료되는 현상 방지 코드 삽입 및 메모가 없을 시 오류 띄우며 원래 화면으로 돌아가는 코드 추가

--- src/Logic.java
+++ src/Logic.java
@@ -5,6 +5,14 @@ public class Logic {
     MemoList memoList = new MemoList();
     Scanner scanner = new Scanner(System.in);
 
+    public void scanIsNum(){
+        while (!scanner.hasNextInt()) {
+            scanner.next();
+            System.err.print("에러! 숫자가 아닙니다. \n다시 입력해주세요 : ");
+        }
+        scanner.nextLine();
+    }
+
     public void insertMemo() {
         System.out.println("==========");
         System.out.print("사용자 명을 입력해주세요 : ");
@@ -15,17 +23,15 @@ public class Logic {
         String createdMemoContent = scanner.nextLine();
 
         System.out.print("비밀번호를 입력해주세요(숫자 네자리) : ");
-        while (!scanner.hasNextInt()) {
-            scanner.next();
-            System.err.print("에러! 숫자가 아닙니다. \n비밀번호를 입력해주세요(숫자 네자리) : ");
-        }
+        scanIsNum();
         int createdUserPassword = scanner.nextInt();
-
         while ((int) (Math.log10(createdUserPassword) + 1) != 4) {
-            System.err.print("비밀번호를 다시 입력해주세요(숫자 네자리) : ");
+            System.err.print ("4자리 숫자가 아닙니다. \n비밀번호를 다시 입력해주세요 : ");
+            scanIsNum();
             createdUserPassword = scanner.nextInt();
         }
-        scanner.nextLine();
+        //nextInt는 enter 값을 반환하지 않기 때문에 버퍼에 남아있어 빼주어야 한다.
+        //그렇지 않으면 이런저런 오류가 남
         // Math.log10(인자)+1 <- (숫자의 자리수를 판별해줌) 을 사용해서 4가 아니면 빠져나오지 않는다.
 
         Memo memo = new Memo(createdUserName, createdUserPassword, createdMemoContent);
@@ -50,8 +56,8 @@ public class Logic {
             System.out.println("==========");
         }
         System.out.print("뒤로 가기를 원하시면 0을 입력해주세요: ");
-        String s1 = scanner.nextLine();
-        if (!s1.equals("0")) {
+        String number = scanner.nextLine();
+        if (!number.equals("0")) {
             showMemoList();
         }
     }
@@ -59,15 +65,21 @@ public class Logic {
     public void modifyMemo() {
         System.out.println("==========");
         System.out.print("수정을 원하는 메모 번호를 입력해주세요 : ");
+        scanIsNum();
         int modifyMemoNum = scanner.nextInt();
         while (memoList.getMemoNum(modifyMemoNum) == null) {
             System.err.print("메모 번호에 맞는 메모가 존재하지 않습니다. \n수정을 원하는 메모 번호를 입력해주세요: ");
+            scanIsNum();
             modifyMemoNum = scanner.nextInt();
         }
+        scanner.nextLine();
         System.out.print("메모 비밀 번호를 입력해주세요 : ");
+        scanIsNum();
         int modifyMemoPassword = scanner.nextInt();
+
         while (memoList.getMemoNum(modifyMemoNum).getUserPassword() != modifyMemoPassword) {
             System.err.print("비밀번호가 일치하지 않습니다. \n다시 입력해주세요 : ");
+            scanIsNum();
             modifyMemoPassword = scanner.nextInt();
         }
         scanner.nextLine();
@@ -78,7 +90,7 @@ public class Logic {
 
         memoList.getMemoNum(modifyMemoNum).setMemoContent(modifyMemoContent);
         memoList.getMemoNum(modifyMemoNum).setModifiedDate();
-        System.out.println("메모가 정상적으로 수정되었습니다.");
+        System.out.println("System: 메모가 정상적으로 수정되었습니다.");
     }
 
     public void deleteMemo() {
@@ -89,19 +101,21 @@ public class Logic {
         System.out.println("==========");
         System.out.print("삭제를 원하는 메모 번호를 입력해주세요: ");
 
+        scanIsNum();
         int deleteNum = scanner.nextInt();
         while (memoList.getMemoNum(deleteNum) == null) {
             System.err.print("메모 번호에 맞는 메모가 존재하지 않습니다. \n삭제를 원하는 메모 번호를 입력해주세요: ");
+            scanIsNum();
             deleteNum = scanner.nextInt();
         }
-
         System.out.print("메모 비밀번호를 입력해주세요: ");
         int deletePassword = scanner.nextInt();
         while (memoList.getMemoNum(deleteNum).getUserPassword() != deletePassword) {
             System.err.print("비밀번호가 일치하지 않습니다. \n다시 입력해주세요 : ");
+            scanIsNum();
             deletePassword = scanner.nextInt();
         }
         memoList.deleteMemo(deleteNum);
-        System.out.println("메모가 삭제되었습니다.");
+        System.out.println("System: 메모가 삭제되었습니다.");
     }
 }
